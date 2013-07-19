ce8ca5b743d4368bfdad16a8bd84246a1daf9279
Emily Priddy <emily.priddy09@gmail.com>
2013-07-19 02:45:11 +0000

    More additions using index.html and web.js

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var hello  = fs.readFileSync('index.html');
+var hello  = fs.readFileSync('index.html','utf-8');
 app.get('/', function(request, response) {
-  response.send(hello.toString('utf-8');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
